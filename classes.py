4ea14aee6b54f5f214d511874fc102f3e6904427
Nazarii Arshynskyi <nazar.arshynskyi@gmail.com>
2023-03-28 20:41:07 +0300

    updated car class

---
+++ classes.py
@@ -0,0 +1,44 @@
+import os
+import pygame
+from math import sin, radians, degrees, copysign
+from pygame.math import Vector2
+import time
+import numpy as np
+from PIL import Image, ImageDraw
+from scipy.interpolate import splprep, splev
+
+
+class Car:
+    def __init__(self, x, y, angle=0, length=2, max_steering=80, max_acceleration=4.0):
+        self.position = Vector2(x, y)
+        self.velocity = Vector2(0.0, 0.0)
+        self.angle = angle
+        self.length = length
+        self.max_acceleration = max_acceleration
+        self.max_steering = max_steering
+        self.max_velocity = 5
+        self.brake_deceleration = 4
+        self.free_deceleration = 1
+
+        self.acceleration = 0.0
+        self.steering = 0.0
+        self.fov = 175  # 150
+        self.turning_sharpness = 1.8
+        self.breaks = True
+        self.fov_range = 60
+        self.auto = False
+        self.headlights = False
+
+    def update(self, dt):
+        self.velocity += (self.acceleration * dt, 0)
+        self.velocity.x = max(-self.max_velocity, min(self.velocity.x, self.max_velocity))
+
+        if self.steering:
+            turning_radius = self.length / sin(radians(self.steering))
+            angular_velocity = self.velocity.x / turning_radius
+        else:
+            angular_velocity = 0
+
+        self.position += self.velocity.rotate(-self.angle) * dt
+        self.angle += degrees(angular_velocity) * dt
+
